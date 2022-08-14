0ce56646f9e5fa9ea270918fafd7cd9fc592c997
Joco Gum <jocogum10@gmail.com>
2022-08-14 19:05:35 +0800

    add issues list view

--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Issue(models.Model):
+    issue_name = models.CharField(max_length=200)
+    body = models.TextField()
+    created_by = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.issue_name
+
+    def get_absolute_url(self):
+        return reverse("issue_detail", kwargs={"pk": self.pk})
--- issues/views.py
+++ issues/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Issue
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'home.html', context)
+
+class IssueListView(ListView):
+    model = Issue
+    template_name = 'issues.html'
